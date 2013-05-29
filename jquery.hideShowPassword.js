24e0f374a5e0adb42ef417ff9a90b4164e40baca
Tyler Sticka <tyler@tylersticka.com>
2013-05-29 14:19:39 -0700

    all functions created and basically working

--- jquery.hideShowPassword.js
+++ jquery.hideShowPassword.js
@@ -1,72 +1,92 @@
 (function ($, window, undef) {
 
+  // TODO:
+  // - do not wipe out old options if el has already been initialized
+  //   (maybe store in $.data?)
+  // - only add styles we need to
+  // - trigger events
+  // - allow mode where no styles are added? (suppressInnerToggleCSS)?
+
   $.fn.hideShowPassword = function (options) {
-    var opts = $.extend({}, $.fn.hideShowPassword.defaults, options);
+    if (typeof options === 'boolean') options = { show: options };
+    var opts = $.extend({}, $.fn.hideShowPassword.defaults, options)
+      , attr = opts.show ? opts.showAttr : opts.hideAttr;
     return this.each(function () {
       var $this = $(this)
         , $wrapper
         , $toggle;
+      $this.attr(attr);
+      $this.trigger(opts.show ? opts.showEvent : opts.hideEvent);
       if (opts.innerToggle) {
         $this.wrap($('<div />').addClass(opts.wrapperClass));
         $wrapper = $this.parent();
-        $toggle = $('<div />').addClass(opts.innerToggleClass).text(
+        $toggle = $('<div />').addClass(opts.toggleClass).text(
           opts.show ? opts.hideText : opts.showText
         );
         $toggle.appendTo($wrapper);
-        $toggle.css({
-          'margin-top': ($toggle.outerHeight() / -2)
-        });
-        $this.css({
-          'padding-right': $toggle.outerWidth()
-        });
-
+        $toggle.css('margin-top', ($toggle.outerHeight() / -2));
         if (opts.touchSupport) {
           $toggle.css('pointer-events', 'none');
           $this.on('touchstart', function (event) {
-            var minLeft = $toggle.offset().left
-              , pageX = (event.pageX === undef) ? event.originalEvent.pageX : event.pageX;
-            if (pageX >= minLeft) {
+            var minX = $toggle.offset().left
+              , curX = event.pageX || event.originalEvent.pageX;
+            if (curX >= minX) {
               event.preventDefault();
-              if ($this.attr('type') === 'password') {
-                $this.attr(opts.textArgs);
-                $toggle.text(opts.hideText);
-              } else {
-                $this.attr(opts.passwordArgs);
-                $toggle.text(opts.showText);
-              }
+              $this.togglePassword();
             }
           });
         } else {
-          $toggle.on('click', function (event) {
-            if ($this.attr('type') === 'password') {
-              $this.attr(opts.textArgs);
-              $toggle.text(opts.hideText);
-            } else {
-              $this.attr(opts.passwordArgs);
-              $toggle.text(opts.showText);
-            }
+          $toggle.on('click', function () {
+            $this.togglePassword();
           });
         }
+        $this.on(opts.showEvent, function () {
+          $toggle.text(opts.hideText);
+        });
+        $this.on(opts.hideEvent, function () {
+          $toggle.text(opts.showText);
+        });
       }
     });
-  };
+  }
+
+  $.fn.showPassword = function (options) {
+    var opts = $.extend({}, options, { show: true });
+    return this.hideShowPassword(opts);
+  }
+
+  $.fn.hidePassword = function (options) {
+    var opts = $.extend({}, options, { show: false });
+    return this.hideShowPassword(opts);
+  }
+
+  $.fn.togglePassword = function (options) {
+    return this.each(function () {
+      var $this = $(this)
+        , opts = $.extend({}, options, { show: ($this.attr('type') === 'password') });
+      $this.hideShowPassword(opts);
+    });
+  }
 
   $.fn.hideShowPassword.defaults = {
-    innerToggle: false,
-    innerToggleClass: 'hideShowPassword-toggle',
-    wrapperClass: 'hideShowPassword-wrapper',
-    showText: 'Show',
-    hideText: 'Hide',
-    touchSupport: false,
     show: false,
-    textArgs: {
+    touchSupport: false,
+    innerToggle: false,
+    wrapperClass: 'hideShowPassword-wrapper',
+    toggleClass: 'hideShowPassword-toggle',
+    showText: 'show',
+    hideText: 'hide',
+    showAttr: {
       'type': 'text',
       'autocorrect': 'off',
       'autocapitalize': 'off'
     },
-    passwordArgs: {
+    hideAttr: {
       'type': 'password'
-    }
+    },
+    showEvent: 'passwordShown',
+    hideEvent: 'passwordHidden',
+    dataOpts: 'hideShowPasswordOptions'
   };
 
 })(jQuery, window);
