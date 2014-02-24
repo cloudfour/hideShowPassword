47daf9b7f8670774b56eb891080b9f5864b52879
Tyler Sticka <tyler@tylersticka.com>
2014-02-24 14:31:02 -0800

    first stab at working test for getSetAttribute

--- hideShowPassword.js
+++ hideShowPassword.js
@@ -242,6 +242,31 @@
 
   };
 
+  // var getSetInputPropSupport = (function(){
+  //   var supported = true
+  //     , testElement = $('<input>')
+  //       .prop('type', 'password')
+  //       .css({
+  //         left: 0,
+  //         height: 1,
+  //         opacity: 0,
+  //         overflow: 'hidden',
+  //         position: 'absolute',
+  //         top: 0,
+  //         width: 1
+  //       })
+  //       .appendTo('body');
+  //   try {
+  //     testElement.prop('type', 'text');
+  //   } catch (error) {
+  //     supported = false;
+  //   }
+  //   // testElement.remove();
+  //   return supported;
+  // }());
+
+  // console.log(getSetInputPropSupport);
+
   $.fn.hideShowPassword = function () {
     var options = (arguments[0] === undef) ? {} : arguments[0];
     if (typeof options !== 'object') {
